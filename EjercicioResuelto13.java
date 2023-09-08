b58cd466029b231689a597716f1953a6d59df1dd
Jhony Alpala <jhoniher95@gmail.com>
2023-09-07 23:26:43 -0500

    Codigo completado

--- EjercicioResuelto13.java
+++ EjercicioResuelto13.java
@@ -6,19 +6,32 @@ public class EjercicioResuelto13 {
         double valor_compra=entrada.nextDouble();
         entrada.nextLine(); //Buffer
         String colorB;
-        while(true){
-            System.out.println("Escriba el color de la bola: \n(blanca/verde/azul/amarilla)");
+        while(true){ //Ciclo para recibir color de bola
+            System.out.println("Escriba el color de la bola: \n(blanca/verde/azul/amarilla/roja)");
             colorB=entrada.nextLine().toLowerCase();
-            if(colorB.equals("blanca") || colorB.equals("verde") || colorB.equals("azul") || colorB.equals("amarilla")){
+            if(colorB.equals("blanca") || colorB.equals("verde") || colorB.equals("azul") || colorB.equals("amarilla") || colorB.equals("roja")){
                 break;
             }
             else{
                 System.out.println("El color ingresado es incorrecto. Intentelo de nuevo.");
             }
         }
-        System.out.println(valor_compra);
-        System.out.println(colorB);
-
-
+        //System.out.println(valor_compra);
+        //System.out.println(colorB);
+        double descuento=0;
+        if(colorB.equals("blanca")){
+            descuento=0;
+        } else if(colorB.equals("verde")) {
+            descuento=valor_compra*0.10;
+        } else if(colorB.equals("amarilla")){
+            descuento=valor_compra*0.25;
+        } else if(colorB.equals("azul")){
+            descuento=valor_compra*0.50;
+        } else if(colorB.equals("roja")){
+            descuento=valor_compra;
+        }
+        double valor_pagar=valor_compra-descuento;
+        System.out.println("Su descuento fue de: "+descuento);
+        System.out.println("El valor a pagar es de: "+valor_pagar);
     }
 }
